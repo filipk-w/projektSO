dc770013a63eb7766714232d68a79c884d48e40e
Filipciatko <filipw2001@gmail.com>
2021-01-05 16:21:35 +0100

    Utworzony plik wyjściowy z danymi Zmiany w generatorze

--- generator.py
+++ generator.py
@@ -1,1 +1,18 @@
-p0 = 
+import random, csv
+from typing import Dict, Any
+
+lista = []
+i = 10  #ilosc procesow do wygenerowania
+for i in range (0, i):
+    lista.append(dict(p=i, at=0, bt=0, wt=0))
+    lista[i]["bt"] = random.randint(1, 10)  # losowanie burst time
+    lista[i]["at"] = random.randint(1, 10)  # losowanie arrival time
+
+print(lista)
+with open('test.csv', 'w') as f:
+    for i in lista:
+        for key in i.keys():
+            f.write("%s,%s\n"%(key,i[key]))
+#arrival time
+#["bt"] = random.randint(1, 10)  #losowanie burst time
+
