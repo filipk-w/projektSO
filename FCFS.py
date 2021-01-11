2eb218fe2c94c841c75a01c1520a20dd1de005c6
filipStacjonarny <filipw2001@gmail.com>
2021-01-12 00:18:20 +0100

    Generator dziala, zmiany w FCFS

--- FCFS.py
+++ FCFS.py
@@ -1,2 +1,30 @@
-while True:
-    queue =
+import csv
+from typing import List, Any
+
+procesy: list[list[Any]] = [[]]
+
+with open('test.csv', newline='') as plikDanych:
+    reader = csv.reader(plikDanych, delimiter=',')
+    counter: int = 0
+    for row in reader:
+        if counter == 0:
+            print(f'Column names are {", ".join(row)}')
+            counter += 1
+        else:
+
+            for i in range(0,3):
+                procesy[counter-1].append(row[i])
+            procesy.append([])
+            counter += 1
+
+
+print(counter-1)
+print(procesy)
+
+globalTimer = 0
+
+#główna pętla programu
+#while True:
+#    currentProcess =
+
+#    globalTimer += 1
--- generator.py
+++ generator.py
@@ -1,23 +1,23 @@
-import random, csv
 import numpy as np
 
-from typing import Dict, Any
-
-i = 10  #ilosc procesow do wygenerowania
+i: int = 10  #ilosc procesow do wygenerowania
 
 p = []
 at = []
 bt = []
-for i in range (0, i):
-    p.append(i)
-    bt.append(np.random.randn(10))
+for a in range(0, i):
+    p.append(a)
+    bt.append(np.random.randint(10))
+    correct = False
+    while not correct:
+        tempAt = np.random.randint(i)
+        if tempAt not in at:
+            at.append(tempAt)
+            correct = True
+        else:
+            correct = False
 
-
-print(lista)
 with open('test.csv', 'w') as f:    #zapisanie danych do pliku
-    for i in lista:
-        for key in i.keys():
-            f.write("%s,%s\n"%(key,i[key]))
-#arrival time
-#["bt"] = random.randint(1, 10)  #losowanie burst time
-
+    f.write("%s,%s,%s\n" % ('pNumber', "burstTime", "arrivalTime"))
+    for i in range (0, i):
+        f.write("%s,%s,%s\n"%(p[i],bt[i],at[i]))
