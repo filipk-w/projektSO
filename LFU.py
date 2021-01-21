a29aaae34fd0e919ce3696502dfae6b0ef180456
filipStacjonarny <filipw2001@gmail.com>
2021-01-21 17:15:54 +0100

    aaaaaaa

--- LFU.py
+++ LFU.py
@@ -14,31 +14,17 @@ for i in referenceString:
         if i not in memory:
             memory[i] = 1
             print(memory)
-            recentlyUsed.append(i)
-            print("Recently used:", recentlyUsed)
             pageFault += 1
         else:
             memory[i] += 1
-            j = recentlyUsed.index(i)
-            del recentlyUsed[j]
-            recentlyUsed.append(i)
-            print("Recently used:", recentlyUsed)
             print("Hit!", memory)
             pageHit +=1
     elif len(memory) >= capacity:
         if i not in memory:
-            del memory[recentlyUsed[0]]
-            del recentlyUsed[0]
-            recentlyUsed.append(i)
             memory[i] = 1
             print(memory)
-            print("Recently used:", recentlyUsed)
             pageFault += 1
         else:
-            j = recentlyUsed.index(i)
-            del recentlyUsed[j]
-            recentlyUsed.append(i)
-            print("Recently used:", recentlyUsed)
             memory[i] += 1
             print("Hit!", memory)
             pageHit += 1
